5c9913cb825a78e950d83aab83dbd134d204417e
xiaobaiha <xiaobaihaha0001@163.com>
2018-07-15 09:00:33 +0800

    @dev remove files

--- App/Themes/Images.js
+++
@@ -1,6 +0,0 @@
-// leave off @2x/@3x
-const images = {
-  placeholder: require('../Images/placeholder.jpg'),
-}
-
-export default images
--- App/Themes/index.js
+++
@@ -1,5 +0,0 @@
-import Images from './Images'
-import Metrics from './Metrics'
-import Colors from './Colors'
-
-export { Images, Metrics, Colors }
