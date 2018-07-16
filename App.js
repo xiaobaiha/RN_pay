642f12f1e12cbfb0ded83f0038581c44a7458b5e
xiaobaiha <xiaobaihaha0001@163.com>
2018-07-16 08:27:01 +0800

    @dev add antd dependcy

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Button} from 'antd-mobile-rn';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>222Open up App.js to start working on your app!</Text>
+        <Button>antd button</Button>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
