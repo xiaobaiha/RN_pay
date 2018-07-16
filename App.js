c8570306d9cb379d3f331733cc8d43140176eba0
xiaobaiha <xiaobaihaha0001@163.com>
2018-07-16 10:22:36 +0800

    @dev frame adjust

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {SearchBar ,TabBar} from 'antd-mobile-rn';
 import { StyleSheet, Text, View } from 'react-native';
+import AKS from './src/pages/AKS';
+import Setting from './src/pages/Setting';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -32,22 +34,24 @@ export default class App extends React.Component {
         barTintColor="#ccc"
       >
       <TabBar.Item
-          icon={require('./busi.png')}
-          selectedIcon={require('./busi_sel.png')}
+          icon={require('./src/styles/imgs/busi.png')}
+          selectedIcon={require('./src/styles/imgs/busi_sel.png')}
           title='一键购物'
           selected={this.state.selectedTab === 'yellowTab'}
           onPress={() => this.onChangeTab('yellowTab')}
         >
-          {this.renderContent('My Tab')}
+          {/* {this.renderContent('My Tab')} */}
+          <AKS />
         </TabBar.Item>
         <TabBar.Item
-          icon={require('./friend.png')}
-          selectedIcon={require('./friend_sel.png')}
+          icon={require('./src/styles/imgs/friend.png')}
+          selectedIcon={require('./src/styles/imgs/friend_sel.png')}
           title="管理"
           selected={this.state.selectedTab === 'greenTab'}
           onPress={() => this.onChangeTab('greenTab')}
         >
-          {this.renderContent('Friend Tab')}
+          {/* {this.renderContent('Friend Tab')} */}
+          <Setting />
         </TabBar.Item>
         
       </TabBar>
---
+++ src/pages/AKS.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SearchBar ,TabBar} from 'antd-mobile-rn';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class AKS extends React.Component {
+  render() {
+    return (
+      <View><Text>AKS</Text></View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
