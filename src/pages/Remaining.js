6eae18029b03a673e6eed53230ff86e0fc8409b6
RedOvO <599236422@qq.com>
2018-07-20 14:31:47 +0800

    @dev: add config

--- src/pages/Remaining.js
+++ src/pages/Remaining.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { preURL } from '../config/axiosConfig'
 
 export default class Pay extends React.Component {
   state = {
@@ -7,6 +9,14 @@ export default class Pay extends React.Component {
   }
   componentWillMount() {
     // axios 获取余额
+    axios({
+      method: "GET",
+      url: preURL + "/balance?userId=1",
+  }).then(response => {
+      console.log(response)
+      //alert(response.data)
+      this.setState({remains: response.data})
+  })
   }
   render() {
     const {remains} = this.state;
