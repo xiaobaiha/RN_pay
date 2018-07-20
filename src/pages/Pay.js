39f0ec8c7c803d51d2f9c11be83eb03981500f0d
xiaobaiha <xiaobaihaha0001@163.com>
2018-07-20 15:06:22 +0800

    @dev app remove comment

--- src/pages/Pay.js
+++ src/pages/Pay.js
@@ -34,7 +34,7 @@ export default class Pay extends React.Component {
     // }, (e) => {
     //   console.log(e);
     // });
-    Alipay.pay("alipay_sdk=alipay-sdk-java-3.3.4.ALL&app_id=2016091900545639&biz_content=%7B%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%2211111%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22App%E6%94%AF%E4%BB%98%E6%B5%8B%E8%AF%95Java%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.01%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=%E5%95%86%E6%88%B7%E5%A4%96%E7%BD%91%E5%8F%AF%E4%BB%A5%E8%AE%BF%E9%97%AE%E7%9A%84%E5%BC%82%E6%AD%A5%E5%9C%B0%E5%9D%80&sign=MYiAP%2Bp5npUgOQ8iK2xk4rlsWtYRJUd4090WuTrt26IMNm4ZP%2BTvvu2nPd26npHk7VGmCKAITg7Dm2%2F9JpchcQXFKXsAFpC6McQcrMdrBw34NaW4VJx8Lfp%2FlE5zs0NhJMGCMCrkgHDgvKboSwyi%2B7tvv%2Fl94%2BnF06RyOzvgJdDlejGdgsQYZRwWsPN4aBF2dZlTF7OEfYDaW4M7XeeYVqFHHspObv8A25Rua87l9axkjvPaXIWh8VOf4SqgzgO7oq8ZVPenfFMLXFtAS340KiUx7rfugnT2JE8U2RTriP19O9orG9GHypA2ux0Zj90pwT%2FBI%2Fu%2FqjDdBVNMecMDFg%3D%3D&sign_type=RSA2&timestamp=2018-07-20+09%3A48%3A02&version=1.0").then(function(data){
+    Alipay.pay(params).then(function(data){
       console.log(data);
     }, function (err) {
         console.log(err);
@@ -43,7 +43,7 @@ export default class Pay extends React.Component {
 
   handlePay = ()=>{
     console.log('enter handlepay')
-    this.pay('alipay_sdk=alipay-sdk-java-3.3.4.ALL&app_id=2016091900545639&biz_content=%7B%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%2211111%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22App%E6%94%AF%E4%BB%98%E6%B5%8B%E8%AF%95Java%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.01%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=%E5%95%86%E6%88%B7%E5%A4%96%E7%BD%91%E5%8F%AF%E4%BB%A5%E8%AE%BF%E9%97%AE%E7%9A%84%E5%BC%82%E6%AD%A5%E5%9C%B0%E5%9D%80&sign=SbpcV75zjoj9puCQtKBVgKNYdVLPPUIJ4OcQhpFcQ3eB%2Fr%2FxvPDHqn%2Frl6bBlYEVL840REqNhLSE0vhaIK6iaTPepoNrBQHJCg6PeBO%2FdGiWTR1Jd5LKXGFcoZkLh1dyah4rYIGNt0QRQ2u27AD6MHL%2Buli1GKe8lhO1biDNN%2BDh%2Bk7onH3tX1M6Q4M1uipup6%2Bcxb41z8mX%2FDdgZ8BgKUT1T%2B48pixRxtB3RkQ%2BhplLRaQtZ0uTRfDp7eYyQGiSj9Y9th1HBs2SpYpyZ3%2BhMVsyo1a6nGaxArTgn2ZqCVlC5pe64eAjyLoTqKbyeAl0TR4ZW1R%2BSaVNEfEUVIZBCQ%3D%3D&sign_type=RSA2&timestamp=2018-07-18+14%3A36%3A17&version=1.0');
+    this.pay('alipay_sdk=alipay-sdk-java-3.3.4.ALL&app_id=2016091900545639&biz_content=%7B%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%221%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22App%E6%94%AF%E4%BB%98%E6%B5%8B%E8%AF%95Java%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.01%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=%E5%95%86%E6%88%B7%E5%A4%96%E7%BD%91%E5%8F%AF%E4%BB%A5%E8%AE%BF%E9%97%AE%E7%9A%84%E5%BC%82%E6%AD%A5%E5%9C%B0%E5%9D%80&sign=FPU8WfFrL5G0HF8tfcehhfjmHjjnhMLI9hN1isJactqp1dUrDpTxKYSOGIDaqZtCuo%2BWghtGGvDhBhHsrbhNaTOpeig8iioiYKy7d%2FrC4Vn2iP87nJcMJTMt2%2Bluuc9CTq7rh8tdN%2B0%2Bw%2BOtwqFAus3hKWcgYL6Vt6xtjj4zViDi70l0qtdxLuBw9Tno6gnY2uxkq0s6GFKJoPzJRFzC6p7D5k0adX7zJ7qYB%2Fc7aZFrUcRwB9N6WDv3USfALrZgV2uSrstHvfrkekn9vtaoVRCeyGUWZkAqRW0ZTdgwHHpC5QDJ2PpH4QXQuaKaSxh9Iq3EJfrKuksWhinhoWsCBA%3D%3D&sign_type=RSA2&timestamp=2018-07-20+14%3A43%3A55&version=1.0');
   }
 
   handleToggle = (e) => {
