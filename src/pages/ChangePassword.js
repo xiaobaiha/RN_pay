5ca88d0e70569d64b9f31940578cd5fc5a6e701a
xiaobaiha <xiaobaihaha0001@163.com>
2018-07-21 11:34:58 +0800

    @dev save change

--- src/pages/ChangePassword.js
+++ src/pages/ChangePassword.js
@@ -46,6 +46,7 @@ export default class ChangePassword extends React.Component {
                   newPassword: value,
                 });
               }}
+              value={this.state.newPassword}
               type='password'
               placeholder="新密码"
             >
