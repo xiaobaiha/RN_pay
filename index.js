8530e1de4abfa749185478ed22f470c278ddb2f6
xiaobaiha <xiaobaihaha0001@163.com>
2018-07-23 14:32:07 +0800

    @dev change app name

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { YellowBox } from 'react-native';
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { YellowBox } from "react-native";
+YellowBox.ignoreWarnings([
+  "Warning: isMounted(...) is deprecated",
+  "Module RCTImageLoader"
+]);
 
-AppRegistry.registerComponent('reactNativeDemo', () => App);
+AppRegistry.registerComponent("WeShop", () => App);
