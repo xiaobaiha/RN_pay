a88893093aef736c3bbb94405e4542146eef8ce6
xiaobaiha <xiaobaihaha0001@163.com>
2018-07-23 16:42:43 +0800

    @dev login style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { StyleSheet, Image, View, Alert, AsyncStorage } from "react-native";
+import { StyleSheet, Image, View, Text, AsyncStorage } from "react-native";
 import { Button, InputItem, Toast, Modal } from "antd-mobile-rn";
 import axios from "axios";
 import { preURL } from "../config/axiosConfig";
+import InputItemStyle from "antd-mobile-rn/lib/input-item/style/index.native";
 
+let newInputItemStyle = {
+  ...InputItemStyle,
+  container: {
+    ...InputItemStyle.container,
+    borderBottomColor: "#3b4757"
+  },
+  input: {
+    ...InputItemStyle.input,
+    backgroundColor: "white",
+    justifyContent: "center",
+    textAlign: "center",
+    height: 40,
+    width: 300,
+    borderRadius: 20
+  }
+};
 export default class Login extends React.Component {
   state = {
     name: "",
     password: "",
-    configLoaded: false,
-    closeFlag: false
+    configLoaded: true,
+    closeFlag: true
   };
   componentWillMount() {
     // async 获取用户信息 若存在，跳转Home
@@ -57,7 +74,6 @@ export default class Login extends React.Component {
         AsyncStorage.setItem("username", response.data.username);
         let TempAddresses = { tempaddresses: [] };
         AsyncStorage.setItem("tempaddresses", JSON.stringify(TempAddresses));
-
         // axios 获取用户一键购物设置
         let UserId = response.data.id;
         axios({
@@ -82,36 +98,46 @@ export default class Login extends React.Component {
     const { configLoaded, closeFlag } = this.state;
     if (configLoaded && closeFlag) {
       return (
-        <View>
-          <InputItem
-            onErrorPress={() => alert("clicked me")}
-            onChange={value => {
-              this.setState({
-                name: value
-              });
-            }}
-            placeholder="用户名"
+        <View style={styles.container}>
+          <Image source={require("../styles/imgs/cart.png")} />
+          <Text style={styles.weshopText}>WeShop</Text>
+          <View style={styles.inputContainer}>
+            <InputItem
+              styles={StyleSheet.create(newInputItemStyle)}
+              onErrorPress={() => alert("clicked me")}
+              onChange={value => {
+                this.setState({
+                  name: value
+                });
+              }}
+              placeholder="用户名"
+              align="middle"
+            />
+            <InputItem
+              styles={StyleSheet.create(newInputItemStyle)}
+              onErrorPress={() => alert("clicked me")}
+              onChange={value => {
+                this.setState({
+                  password: value
+                });
+              }}
+              type="password"
+              placeholder="密码"
+            />
+          </View>
+          <Button
+            style={styles.loginBtn}
+            type="primary"
+            onClick={this.handleLogin}
           >
-            用户名
-          </InputItem>
-          <InputItem
-            onErrorPress={() => alert("clicked me")}
-            onChange={value => {
-              this.setState({
-                password: value
-              });
-            }}
-            type="password"
-            placeholder="密码"
-          >
-            密码
-          </InputItem>
-          <Button type="primary" onClick={this.handleLogin}>
             登录
           </Button>
-          <Button onClick={() => this.props.navigation.navigate("Signup")}>
+          <Text
+            style={styles.regText}
+            onPress={() => this.props.navigation.navigate("Signup")}
+          >
             没有账号？去注册吧
-          </Button>
+          </Text>
         </View>
       );
     } else {
@@ -128,20 +154,44 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#3b4757",
     alignItems: "center",
     justifyContent: "center"
   },
+  input: {
+    backgroundColor: "white",
+    height: 40,
+    width: 300,
+    borderRadius: 20,
+    alignItems: "center"
+  },
+  inputContainer: {
+    height: 130,
+    justifyContent: "space-around",
+    width: 300
+  },
+  weshopText: {
+    margin: 20,
+    color: "#949fb1",
+    fontSize: 30
+  },
+  regText: {
+    color: "#949fb1",
+    fontSize: 20,
+    margin: 10
+  },
+  loginBtn: {
+    height: 40,
+    width: 270,
+    borderRadius: 20,
+    margin: 10
+  },
   launchimageStyle: {
-    // width: deviceWidth, //设备宽(只是一种实现，此处多余)
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     width: null,
     width: null,
-    //不加这句，就是按照屏幕高度自适应
-    //加上这几，就是按照屏幕自适应
     resizeMode: Image.resizeMode.cover
-    //祛除内部元素的白色背景
   }
 });
